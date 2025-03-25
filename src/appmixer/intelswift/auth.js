edc1fa9cbf7167528a2d055f34310980160f535d
vladimir talas <vladimir.talas@gmail.com>
2025-03-25 10:59:30 +0100

    update

---
+++ src/appmixer/intelswift/auth.js
@@ -0,0 +1,51 @@
+const { makeRequest } = require('./commons');
+
+module.exports = {
+
+    type: 'apiKey',
+
+    definition: {
+
+        accountNameFromProfileInfo: 'displayName',
+
+        customFields: {
+            apiKey: {
+                type: 'text',
+                name: 'API Key custom fields',
+                required: true,
+                tooltip: 'Enter your IntelSwift API key custom fields.'
+            }
+        },
+
+        auth: {
+            apiKey: {
+                type: 'text',
+                name: 'API Key',
+                required: true,
+                tooltip: 'Enter your IntelSwift API key.'
+            }
+        },
+
+        requestProfileInfo: async context => {
+
+            const { data } = await makeRequest(context, '/api/v1/accounts/account', { method: 'POST' });
+
+            if (!data?.data) {
+                throw 'Authentication Failed';
+            }
+
+            const account = data?.data[0];
+
+            return {
+                tenantId: account.tenantId,
+                displayName: account.email
+            };
+        },
+
+        validate: async context => {
+
+            const { data } = await makeRequest(context, '/api/v1/accounts/account', { method: 'POST' });
+            return !!data?.data;
+        }
+    }
+};
