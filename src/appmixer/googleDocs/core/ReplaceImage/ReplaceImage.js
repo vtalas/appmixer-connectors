e8cecd4ab0100ac29421cc60353e92d75be1d1b3
vladimir talas <vladimir.talas@gmail.com>
2025-10-09 08:25:08 +0200

    update

--- src/appmixer/googleDocs/core/ReplaceImage/ReplaceImage.js
+++ src/appmixer/googleDocs/core/ReplaceImage/ReplaceImage.js
@@ -1,9 +1,21 @@
 'use strict';
 
 module.exports = {
+
     async receive(context) {
 
         const { documentId, targetImageId, newImageUrl } = context.messages.in.content;
+
+        // Validate required inputs
+        if (!documentId) {
+            throw new context.CancelError('Document Id is required!');
+        }
+        if (!targetImageId) {
+            throw new context.CancelError('Target Image Id is required!');
+        }
+        if (!newImageUrl) {
+            throw new context.CancelError('New Image Url is required!');
+        }
 
         const requests = [{
             replaceImage: {
