44ac445ec80ec6ab310c1b687412bf266d625318
vladimir talas <vladimir.talas@gmail.com>
2025-10-15 20:31:20 +0200

    update

--- src/appmixer/salesforce/auth.js
+++ src/appmixer/salesforce/auth.js
@@ -71,8 +71,8 @@ module.exports = {
                 const { data } = await context.httpRequest({
                     method: 'GET',
                     url: instanceId,
-                    auth: {
-                        bearer: context.accessToken
+                    headers: {
+                        'Authorization': `Bearer ${context.accessToken}`
                     }
                 });
 
