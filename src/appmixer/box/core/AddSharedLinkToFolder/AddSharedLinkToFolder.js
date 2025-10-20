f45df40e0d11066d64edbee51f7e7ebd732557d9
vladimir talas <vladimir.talas@gmail.com>
2025-10-20 17:31:28 +0200

    update

--- src/appmixer/box/core/AddSharedLinkToFolder/AddSharedLinkToFolder.js
+++ src/appmixer/box/core/AddSharedLinkToFolder/AddSharedLinkToFolder.js
@@ -1,23 +1,54 @@
-
 'use strict';
 
-const lib = require('../../lib.generated');
 module.exports = {
-    async receive(context) {        
 
-        const { folder_id, shared_link|access, shared_link|password, shared_link|unshared_at, shared_link|permissions|can_download, shared_link|permissions|can_preview } = context.messages.in.content;
+    async receive(context) {
 
+        const { folderId, access, password, unsharedAt, canDownload, canPreview } = context.messages.in.content;
+
+        if (!folderId) {
+            throw new context.CancelError('Folder ID is required.');
+        }
+
+        // Build the shared_link object
+        const sharedLink = {};
+
+        if (access) {
+            sharedLink.access = access;
+        }
+
+        if (password) {
+            sharedLink.password = password;
+        }
+
+        if (unsharedAt) {
+            sharedLink.unshared_at = unsharedAt;
+        }
+
+        // Build permissions object if any permission is specified
+        if (canDownload !== undefined || canPreview !== undefined) {
+            sharedLink.permissions = {};
+            if (canDownload !== undefined) {
+                sharedLink.permissions.can_download = canDownload;
+            }
+            if (canPreview !== undefined) {
+                sharedLink.permissions.can_preview = canPreview;
+            }
+        }
 
         // https://developer.box.com/guides/shared-links/create/
         const { data } = await context.httpRequest({
             method: 'PUT',
-            url: 'https://api.box.com/2.0/folders/{folder_id}',
+            url: `https://api.box.com/2.0/folders/${folderId}`,
             headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
+                'Authorization': `Bearer ${context.auth.accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            data: {
+                shared_link: sharedLink
             }
         });
-    
 
-return context.sendJson(data, 'out');
+        return context.sendJson(data, 'out');
     }
 };
--- src/appmixer/box/core/CancelSignRequest/CancelaSignRequest.js
+++ src/appmixer/box/core/CancelSignRequest/CancelaSignRequest.js
@@ -1,21 +1,26 @@
-
 'use strict';
 
 const lib = require('../../lib.generated');
+
 module.exports = {
+
     async receive(context) {
 
         const { sign_request_id } = context.messages.in.content;
 
+        if (!sign_request_id) {
+            throw new context.CancelError('Sign Request ID is required.');
+        }
+
         // https://developer.box.com/reference/post-sign-requests-id-cancel/
-        const { data } = await context.httpRequest({
+        await context.httpRequest({
             method: 'POST',
-            url: 'https://api.box.com/2.0/sign_requests/{sign_request_id}/cancel',
+            url: `https://api.box.com/2.0/sign_requests/${sign_request_id}/cancel`,
             headers: {
                 'Authorization': `Bearer ${context.auth.apiToken}`
             }
         });
 
-        return context.sendJson(data, 'out');
+        return context.sendJson({}, 'out');
     }
 };
