a1870ef080751ca3edc37cff9c47d078110f8a18
vladimir talas <vladimir.talas@gmail.com>
2025-10-31 14:12:02 +0100

    update

--- src/appmixer/box/core/GetFile/GetFile.js
+++ src/appmixer/box/core/GetFile/GetFile.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const lib = require('../../lib.generated');
-
 module.exports = {
 
     async receive(context) {
 
-        const { file_id, fields } = context.messages.in.content;
+        const { fileId, fields } = context.messages.in.content;
 
-        if (!file_id) {
+        if (!fileId) {
             throw new context.CancelError('File ID is required!');
         }
 
@@ -21,7 +19,7 @@ module.exports = {
         // https://developer.box.com/reference/get-files-id/
         const { data } = await context.httpRequest({
             method: 'GET',
-            url: `https://api.box.com/2.0/files/${file_id}`,
+            url: `https://api.box.com/2.0/files/${fileId}`,
             headers: {
                 'Authorization': `Bearer ${context.auth.accessToken}`
             },
