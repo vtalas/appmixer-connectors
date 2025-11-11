e7b3d85f250cfd7427c98208a94b0a5b3c77d215
vladimir talas <vladimir.talas@gmail.com>
2025-11-11 11:10:20 +0100

    paypal init

--- src/appmixer/paypal/core/CaptureOrder/CaptureOrder.js
+++ src/appmixer/paypal/core/CaptureOrder/CaptureOrder.js
@@ -1,19 +1,63 @@
-
 'use strict';
 
-const lib = require('../../lib');
 module.exports = {
     async receive(context) {
 
         const { order_id, paypal_request_id, note_to_payer, payment_source } = context.messages.in.content;
 
+        if (!order_id) {
+            throw new context.CancelError('Order ID is required!');
+        }
+
+        // Get client credentials and environment from auth
+        const { clientId, clientSecret, environment } = context.auth;
+        const baseUrl = environment === 'live' ? 'https://api.paypal.com' : 'https://api.sandbox.paypal.com';
+
+        // Get access token
+        const tokenResponse = await context.httpRequest({
+            method: 'POST',
+            url: `${baseUrl}/v1/oauth2/token`,
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Language': 'en_US',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            auth: {
+                username: clientId,
+                password: clientSecret
+            },
+            data: 'grant_type=client_credentials'
+        });
+
+        const accessToken = tokenResponse.data.access_token;
+        if (!accessToken) {
+            throw new context.CancelError('Could not obtain PayPal access token.');
+        }
+
+        // Build request body
+        const requestBody = {};
+        if (paypal_request_id) {
+            requestBody.paypal_request_id = paypal_request_id;
+        }
+        if (note_to_payer) {
+            requestBody.note_to_payer = note_to_payer;
+        }
+        if (payment_source) {
+            requestBody.payment_source = payment_source;
+        }
+
+        // Capture the order
         // https://developer.paypal.com/docs/api/orders/v2/#orders_capture
         const { data } = await context.httpRequest({
             method: 'POST',
-            url: 'https://api-m.sandbox.paypal.com/v2/checkout/orders/{order_id}/capture',
+            url: `${baseUrl}/v2/checkout/orders/${order_id}/capture`,
             headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
-            }
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+                'PayPal-Auth-Assertion': '',
+                'Prefer': 'return=representation'
+            },
+            data: requestBody
         });
 
         return context.sendJson(data, 'out');
--- src/appmixer/paypal/core/CreateBatchPayout/CreateBatchPayout.js
+++ src/appmixer/paypal/core/CreateBatchPayout/CreateBatchPayout.js
@@ -1,23 +1,91 @@
-
 'use strict';
 
-const lib = require('../../lib');
 module.exports = {
-    async receive(context) {        
+    async receive(context) {
+        const {
+            senderBatchId,
+            emailSubject,
+            emailMessage,
+            recipientType,
+            receiver,
+            currency,
+            value,
+            note,
+            senderItemId
+        } = context.messages.in.content;
 
-        const { sender_batch_header|sender_batch_id, sender_batch_header|email_subject, sender_batch_header|email_message, items|recipient_type, items|receiver, items|amount|currency, items|amount|value, items|note, items|sender_item_id } = context.messages.in.content;
+        // Validate required inputs
+        if (!senderBatchId) {
+            throw new context.CancelError('Sender Batch ID is required!');
+        }
+        if (!recipientType) {
+            throw new context.CancelError('Recipient Type is required!');
+        }
+        if (!receiver) {
+            throw new context.CancelError('Receiver is required!');
+        }
+        if (!currency) {
+            throw new context.CancelError('Currency is required!');
+        }
+        if (!value) {
+            throw new context.CancelError('Amount is required!');
+        }
 
+        // Get access token using client credentials
+        const baseUrl = context.auth.environment === 'live' ? 'https://api.paypal.com' : 'https://api.sandbox.paypal.com';
+
+        const tokenResponse = await context.httpRequest({
+            method: 'POST',
+            url: `${baseUrl}/v1/oauth2/token`,
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Language': 'en_US',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            auth: {
+                username: context.auth.clientId,
+                password: context.auth.clientSecret
+            },
+            data: 'grant_type=client_credentials'
+        });
+
+        const accessToken = tokenResponse.data.access_token;
+        if (!accessToken) {
+            throw new Error('Could not obtain PayPal access token.');
+        }
+
+        // Build the payout request
+        const payoutRequest = {
+            sender_batch_header: {
+                sender_batch_id: senderBatchId,
+                email_subject: emailSubject || 'Payout Notification',
+                email_message: emailMessage || 'You have received a payout!'
+            },
+            items: [
+                {
+                    recipient_type: recipientType,
+                    receiver: receiver,
+                    amount: {
+                        currency: currency,
+                        value: value
+                    },
+                    note: note,
+                    sender_item_id: senderItemId
+                }
+            ]
+        };
 
         // https://developer.paypal.com/docs/api/payments.payouts-batch/v1/#payouts_post
         const { data } = await context.httpRequest({
             method: 'POST',
-            url: 'https://api-m.sandbox.paypal.com/v1/payments/payouts',
+            url: `${baseUrl}/v1/payments/payouts`,
             headers: {
-                'Authorization': `Bearer ${context.auth.apiToken}`
-            }
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            data: payoutRequest
         });
-    
 
-return context.sendJson(data, 'out');
+        return context.sendJson(data, 'out');
     }
 };
