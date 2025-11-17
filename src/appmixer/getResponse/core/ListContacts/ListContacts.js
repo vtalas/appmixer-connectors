5f53001ca97b67c2d3b0c1cc8e7c1dfd93ed196c
Sayam Nasir <sayam.nasir53@gmail.com>
2025-11-17 13:44:33 +0500

    lint

--- src/appmixer/getResponse/core/ListContacts/ListContacts.js
+++ src/appmixer/getResponse/core/ListContacts/ListContacts.js
@@ -3,79 +3,79 @@
 const lib = require('../../lib');
 
 const schema = {
-  'contactId': { 'type': 'string', 'title': 'Contact Id' },
-  'name': { 'type': 'string', 'title': 'Name' },
-  'email': { 'type': 'string', 'title': 'Email' },
-  'state': { 'type': 'string', 'title': 'State' },
-  'dayOfCycle': { 'type': 'number', 'title': 'Day Of Cycle' },
-  'campaign': { 'type': 'object', 'properties': { 'campaignId': { 'type': 'string', 'title': 'Campaign.Campaign Id' } }, 'title': 'Campaign' },
-  'ipAddress': { 'type': 'string', 'title': 'Ip Address' },
-  'createdOn': { 'type': 'string', 'title': 'Created On' },
-  'origin': { 'type': 'string', 'title': 'Origin' },
-  'scoring': { 'type': 'number', 'title': 'Scoring' },
-  'customFieldValues': { 'type': 'array', 'items': { 'type': 'object', 'properties': { 'customFieldId': { 'type': 'string', 'title': 'Custom Field Values.Custom Field Id' }, 'value': { 'type': 'array', 'items': { 'type': 'string' }, 'title': 'Custom Field Values.Value' } } }, 'title': 'Custom Field Values' },
-  'tags': { 'type': 'array', 'items': { 'type': 'object', 'properties': { 'tagId': { 'type': 'string', 'title': 'Tags.Tag Id' }, 'name': { 'type': 'string', 'title': 'Tags.Name' } } }, 'title': 'Tags' }
+    'contactId': { 'type': 'string', 'title': 'Contact Id' },
+    'name': { 'type': 'string', 'title': 'Name' },
+    'email': { 'type': 'string', 'title': 'Email' },
+    'state': { 'type': 'string', 'title': 'State' },
+    'dayOfCycle': { 'type': 'number', 'title': 'Day Of Cycle' },
+    'campaign': { 'type': 'object', 'properties': { 'campaignId': { 'type': 'string', 'title': 'Campaign.Campaign Id' } }, 'title': 'Campaign' },
+    'ipAddress': { 'type': 'string', 'title': 'Ip Address' },
+    'createdOn': { 'type': 'string', 'title': 'Created On' },
+    'origin': { 'type': 'string', 'title': 'Origin' },
+    'scoring': { 'type': 'number', 'title': 'Scoring' },
+    'customFieldValues': { 'type': 'array', 'items': { 'type': 'object', 'properties': { 'customFieldId': { 'type': 'string', 'title': 'Custom Field Values.Custom Field Id' }, 'value': { 'type': 'array', 'items': { 'type': 'string' }, 'title': 'Custom Field Values.Value' } } }, 'title': 'Custom Field Values' },
+    'tags': { 'type': 'array', 'items': { 'type': 'object', 'properties': { 'tagId': { 'type': 'string', 'title': 'Tags.Tag Id' }, 'name': { 'type': 'string', 'title': 'Tags.Name' } } }, 'title': 'Tags' }
 };
 
 module.exports = {
-  async receive(context) {
+    async receive(context) {
 
-    const {
-      email,
-      name,
-      campaignId,
-      state,
-      createdOnFrom,
-      createdOnTo,
-      sortBy,
-      sortOrder,
-      outputType
-    } = context.messages.in.content;
+        const {
+            email,
+            name,
+            campaignId,
+            state,
+            createdOnFrom,
+            createdOnTo,
+            sortBy,
+            sortOrder,
+            outputType
+        } = context.messages.in.content;
 
-    if (context.properties.generateOutputPortOptions) {
-      return lib.getOutputPortOptions(context, outputType, schema, { label: 'Items', value: 'contacts' });
-    }
+        if (context.properties.generateOutputPortOptions) {
+            return lib.getOutputPortOptions(context, outputType, schema, { label: 'Items', value: 'contacts' });
+        }
 
-    // Build query parameters
-    const params = {};
+        // Build query parameters
+        const params = {};
 
-    if (email) {
-      params['query[email]'] = email;
-    }
-    if (name) {
-      params['query[name]'] = name;
-    }
-    if (campaignId) {
-      params['query[campaignId]'] = campaignId;
-    }
-    if (state) {
-      params['query[state]'] = state;
-    }
-    if (createdOnFrom) {
-      params['query[createdOn][from]'] = createdOnFrom;
-    }
-    if (createdOnTo) {
-      params['query[createdOn][to]'] = createdOnTo;
-    }
-    if (sortBy) {
-      params['sort[' + sortBy + ']'] = sortOrder || 'asc';
-    }
+        if (email) {
+            params['query[email]'] = email;
+        }
+        if (name) {
+            params['query[name]'] = name;
+        }
+        if (campaignId) {
+            params['query[campaignId]'] = campaignId;
+        }
+        if (state) {
+            params['query[state]'] = state;
+        }
+        if (createdOnFrom) {
+            params['query[createdOn][from]'] = createdOnFrom;
+        }
+        if (createdOnTo) {
+            params['query[createdOn][to]'] = createdOnTo;
+        }
+        if (sortBy) {
+            params['sort[' + sortBy + ']'] = sortOrder || 'asc';
+        }
 
-    // Retrieve contacts from getResponse API
-    // https://apireference.getresponse.com/#contacts
-    const response = await context.httpRequest({
-      method: 'GET',
-      url: 'https://api.getresponse.com/v3/contacts',
-      headers: {
-        'X-Auth-Token': `api-key ${context.auth.apiKey}`,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      params
-    });
+        // Retrieve contacts from getResponse API
+        // https://apireference.getresponse.com/#contacts
+        const response = await context.httpRequest({
+            method: 'GET',
+            url: 'https://api.getresponse.com/v3/contacts',
+            headers: {
+                'X-Auth-Token': `api-key ${context.auth.apiKey}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            params
+        });
 
-    const contacts = response.data.contacts || [];
+        const contacts = response.data.contacts || [];
 
-    return lib.sendArrayOutput({ context, records: contacts, outputType });
-  }
+        return lib.sendArrayOutput({ context, records: contacts, outputType });
+    }
 };
