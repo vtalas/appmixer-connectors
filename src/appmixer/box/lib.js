c2947ec0eadc97a87f70724d8e2edb4f885d7f6a
vladimir talas <lades@client.io>
2026-01-21 16:05:21 +0100

    Update src/appmixer/box/lib.js
    Co-authored-by: Jiří Hofman <jiri.hofman@gmail.com>

--- src/appmixer/box/lib.js
+++ src/appmixer/box/lib.js
@@ -50,10 +50,6 @@ module.exports = {
         }
     },
 
-    getProperty(obj, path) {
-        return path.split('.').reduce((acc, part) => acc?.[part], obj);
-    },
-
     getOutputPortOptions(context, outputType, itemSchema, { label }) {
 
         if (outputType === 'object' || outputType === 'first') {
