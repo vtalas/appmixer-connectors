267cc7b9c1eafba79a64f97f0765b00045b927a0
Vladimir Talas <vladimir.talas@appmixer.ai>
2026-01-23 16:55:27 +0100

    update

--- src/appmixer/google/drive/Tick/Tick.js
+++ src/appmixer/google/drive/Tick/Tick.js
@@ -1,9 +1,10 @@
+const lib = require("../lib");
 module.exports = {
 
     async start(context) {
 
         await context.saveState({
-            expirationTime: Date.now() + 60 * 54 * 1000
+            expirationTime: Date.now() + 60 * 50 * 1000 // 50 minutes
         });
 
     },
@@ -25,14 +26,26 @@ module.exports = {
         const now = Date.now();
 
         if (now >= expirationTime) {
+
+            const newExpirationTime = expirationTime + 60 * 61 * 1000;
             await context.log({
                 'step': 'token info ' + accessToken.substr(accessToken.length - 10),
                 expirationTime: new Date(expirationTime),
                 a: context.auth
             });
 
+            const response = await context.httpRequest({
+                method: 'GET',
+                url: 'https://www.googleapis.com/oauth2/v2/userinfo',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            await context.log({ 'step': 'test rq', response: response.data });
+
             await context.saveState({
-                expirationTime: Date.now() + 60 * 54 * 1000
+                expirationTime: newExpirationTime
             });
         }
 
