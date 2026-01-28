23c53cd47927809ec20dd6822564cb928fb5a75e
vladimir talas <vladimir.talas@appmixer.ai>
2026-01-28 15:42:38 +0100

    Box: fixes after testing (#945)

--- src/appmixer/box/core/CreateFolder/CreateFolder.js
+++ src/appmixer/box/core/CreateFolder/CreateFolder.js
@@ -10,22 +10,29 @@ module.exports = {
             throw new context.CancelError('Name is required!');
         }
 
-        // https://developer.box.com/reference/post-folders/
-        const { data } = await context.httpRequest({
-            method: 'POST',
-            url: 'https://api.box.com/2.0/folders',
-            headers: {
-                'Authorization': `Bearer ${context.auth.accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            data: {
-                name,
-                parent: {
-                    id: parentId
+        try {
+            // https://developer.box.com/reference/post-folders/
+            const { data } = await context.httpRequest({
+                method: 'POST',
+                url: 'https://api.box.com/2.0/folders',
+                headers: {
+                    'Authorization': `Bearer ${context.auth.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    name,
+                    parent: {
+                        id: parentId
+                    }
                 }
-            }
-        });
+            });
 
-        return context.sendJson(data, 'out');
+            return context.sendJson(data, 'out');
+        } catch (error) {
+            if (error.status === 409) {
+                throw new context.CancelError('Folder already exists');
+            }
+            throw error;
+        }
     }
 };
