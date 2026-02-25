4edf9649edf9039d3bb13d3fe974ec98174740ed
Vladimir Talas <vladimir.talas@appmixer.ai>
2026-02-25 11:45:05 +0100

    update
    # Conflicts:
    #	src/appmixer/salesforce/crm/CreateLead/component.json
    #	src/appmixer/salesforce/crm/NewLead/component.json

--- src/appmixer/salesforce/crm/NewLead/NewLead.js
+++ src/appmixer/salesforce/crm/NewLead/NewLead.js
@@ -42,6 +42,8 @@ module.exports = {
         }
 
         await Promise.map(diff, lead => {
+
+            context.log({ 'step': 'new lead', lead });
             return context.sendJson(lead, 'lead');
         });
 
